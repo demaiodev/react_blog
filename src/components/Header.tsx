0ba2eaf526249d623e4446d7fb2c62c6c5ddd1ca
demaiodev <fml1041@gmail.com>
2025-11-21 17:57:36 -0500

    style changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,25 @@ export default function Header() {
     <header className="bg-white shadow-sm">
       <div className="max-w-5xl mx-auto p-4 flex items-center justify-between">
         <Link to="/" className="text-2xl font-semibold text-slate-800">
-          CiviBlog
+          <span className="mr-2 bg-slate-800 text-white p-2 rounded">CB</span>
+          <span>CiviBlog</span>
         </Link>
         <nav className="flex items-center gap-3">
           <Link
             to="/"
-            className="text-slate-600 hover:text-slate-900 px-3 py-2 rounded-md"
+            className="text-slate-800 hover:text-slate-600 px-3 py-2 rounded-md"
           >
             Home
           </Link>
           <Link
             to="/moderation"
-            className="text-slate-600 hover:text-slate-900 px-3 py-2 rounded-md"
+            className="text-slate-800 hover:text-slate-600 px-3 py-2 rounded-md"
           >
             Moderation
           </Link>
           <Link
             to="/create"
-            className="bg-slate-600 text-white p-2 rounded-md hover:bg-slate-900"
+            className="bg-slate-800 text-white p-2 rounded-md hover:bg-slate-600"
           >
             + New Post
           </Link>
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,7 +4,7 @@ import type { Post } from "../api/posts";
 export default function PostCard({ post }: { post: Post }) {
   return (
     <Link to={`/posts/${post.id}`}>
-      <article className="bg-white rounded-lg shadow-sm hover:shadow-lg transition space-y-4 p-4 min-h-50 rounded-br-3xl border-gray-300 flex flex-col">
+      <article className="bg-white rounded-lg shadow-sm hover:shadow-lg transition space-y-4 p-4 min-h-50 rounded-br-3xl border border-slate-200 flex flex-col">
         <h3 className="text-lg font-semibold text-slate-900 line-clamp-2">
           {post.title}
         </h3>
@@ -14,7 +14,7 @@ export default function PostCard({ post }: { post: Post }) {
               ? new Date(post.published_date).toLocaleString()
               : ""}
           </div>
-          <div className="text-slate-500">Read Post →</div>
+          <div className="text-slate-500">Read →</div>
         </div>
       </article>
     </Link>
